9824215c4f54ac12cf95cebbd0045d65ac23b7df
ItsNikolor <55848097+ItsNikolor@users.noreply.github.com>
2020-05-05 17:11:38 +0300

    Add files via upload

--- src/concurrency/Executor.cpp
+++ src/concurrency/Executor.cpp
@@ -4,46 +4,52 @@ namespace Afina {
 namespace Concurrency {
 Executor::Executor(size_t low_watermark, size_t hight_watermark, size_t max_queue_size, size_t idle_time)
     : low_watermark(low_watermark), hight_watermark(hight_watermark), max_queue_size(max_queue_size),
-      state(Executor::State::kRun), idle_time(std::chrono::milliseconds(idle_time)) {
+      state(Executor::State::kRun), idle_time(std::chrono::milliseconds(idle_time)), threads_count(low_watermark),
+      busy_count(low_watermark) {
     for (int i = 0; i < low_watermark; i++) {
         auto t = std::thread([this]() { perform(this); });
         t.detach();
-        threads.emplace(std::make_pair(t.get_id(), std::move(t)));
     }
 }
 
 void perform(Executor *executor) {
-    while (executor->state != Executor::State::kStopped) {
+    while (true) {
         std::unique_lock<std::mutex> l(executor->mutex);
-        executor->spare_count++;
+
+        if (executor->tasks.empty()) {
+            executor->busy_count--;
+        }
 
         while (true) {
             auto until = std::chrono::high_resolution_clock::now() + executor->idle_time;
-            while (std::chrono::high_resolution_clock::now() < until && executor->tasks.empty() &&
-                   executor->state == Executor::State::kRun) {
-                executor->empty_condition.wait_until(l, until);
+            while (executor->tasks.empty() && executor->state == Executor::State::kRun) {
+                if (executor->empty_condition.wait_until(l, until) == std::cv_status::timeout) {
+                    break;
+                }
             }
 
-            if (!executor->tasks.empty())
+            if (!executor->tasks.empty()) {
                 break;
+            }
 
             if (executor->state != Executor::State::kRun) {
-                executor->threads.erase(std::this_thread::get_id());
-                if (executor->threads.empty()) {
+                executor->threads_count--;
+                if (executor->threads_count == 0) {
                     executor->state = Executor::State::kStopped;
                     l.unlock();
 
-                    executor->empty_condition.notify_one(); //Спросить!
+                    executor->stop_condition.notify_all();
                 }
+                l.unlock();
+                executor->empty_condition.notify_one();
                 return;
             }
 
-            if (executor->threads.size() == executor->low_watermark)
+            if (executor->threads_count == executor->low_watermark)
                 continue;
 
             // Redundant thread
-            executor->threads.erase(std::this_thread::get_id());
-            executor->spare_count--;
+            executor->threads_count--;
             return;
         }
 
@@ -57,10 +63,14 @@ void perform(Executor *executor) {
 
 void Executor::Stop(bool await) {
     std::unique_lock<std::mutex> lock(mutex);
-    state = State::kStopping;
-    empty_condition.notify_all();
-    while (await && state == State::kStopping) {
-        empty_condition.wait(lock);
+    if (threads_count != 0) {
+        state = State::kStopping;
+    } else {
+        state = State::kStopped;
+    }
+    empty_condition.notify_one();
+    while (await && threads_count) {
+        stop_condition.wait(lock);
     }
 }
 
