6befe088c89e6b489face44db439b2c5008b899c
ItsNikolor <55848097+ItsNikolor@users.noreply.github.com>
2020-06-23 00:58:21 +0300

    Add files via upload

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,56 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
+void Engine::Store(context &ctx) {
+    char StackEnd;
+    ctx.Hight = &StackEnd;
+    size_t stack_size = abs(ctx.Hight - this->StackBottom + 1);
+    char *stack_adr = new char[stack_size];
+    memcpy(stack_adr, this->StackBottom, stack_size);
 
-void Engine::Restore(context &ctx) {}
+    auto old_stack = std::get<0>(ctx.Stack);
+    if (old_stack) {
+        delete old_stack;
+    }
 
-void Engine::yield() {}
+    ctx.Stack = std::make_tuple(stack_adr, stack_size);
+}
 
-void Engine::sched(void *routine_) {}
+void Engine::Restore(context &ctx) {
+    char StackEnd;
+    if ((&StackEnd >= this->StackBottom && &StackEnd <= ctx.Hight) ||
+        (&StackEnd <= this->StackBottom && &StackEnd >= ctx.Hight)) {
+        Restore(ctx);
+    }
+    memcpy(this->StackBottom, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
+    longjmp(ctx.Environment, 1);
+}
+
+void Engine::yield() {
+    if (alive->next == nullptr) {
+        return;
+    }
+    if (setjmp(cur_routine->Environment) > 0) {
+        return;
+    }
+    Store(*cur_routine);
+    Restore(*alive->next);
+}
+
+void Engine::sched(void *routine_) {
+    if (routine_ == nullptr) {
+        yield();
+    }
+    if (routine_ == cur_routine) {
+        return;
+    }
+
+    if (setjmp(cur_routine->Environment) > 0) {
+        return;
+    }
+    Store(*cur_routine);
+    Restore(*static_cast<context *>(routine_));
+}
 
 } // namespace Coroutine
 } // namespace Afina
